f5e8d7078d649a0e5be2ba51ce759135c11d2981
Sabir Imanov <mizixxxx@gmail.com>
2019-01-11 04:10:45 +0400

    Task #4
    Added statistics on homepage

--- src/Model/User.php
+++ src/Model/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Snowdog\DevTest\Model;
+use Snowdog\DevTest\Core\Database;
 
 class User
 {
@@ -9,10 +10,16 @@ class User
     public $password_hash;
     public $password_salt;
     public $display_name;
+    public $total_visited_pages;
+    public $most_recent_visited;
+    public $least_recent_visited;
 
     public function __construct()
     {
         $this->user_id = intval($this->user_id);
+        $this->total_visited_pages = self::getUserVisitedPagesTotal($this->user_id);
+        $this->most_recent_visited= self::getRecentVisitedPage($this->user_id, "most");
+        $this->least_recent_visited = self::getRecentVisitedPage($this->user_id, "least");
     }
 
     /**
@@ -54,6 +61,63 @@ class User
     {
         return $this->display_name;
     }
-    
 
+    /**
+     * @return int
+     */
+    public static function getUserVisitedPagesTotal($user_id)
+    {
+        $db = new Database();
+
+        $select_query = $db->query("SELECT COUNT(*) AS count FROM `pages` p JOIN `websites` w ON w.website_id = p.website_id WHERE w.user_id = '$user_id'");
+        $data = $select_query->fetch();
+
+        $total = $data['count'];
+        return $total;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getRecentVisitedPage($user_id, $ml_type)
+    {
+
+        if ($ml_type === "most") {
+            $order = "DESC";
+        } else {
+            $order = "ASC";
+        }
+
+        $db = new Database();
+
+        $select_query = $db->query("SELECT CONCAT(w.hostname, '/', p.url) AS url, w.name FROM `pages` p LEFT JOIN `websites` w ON w.website_id = p.website_id WHERE w.user_id = '$user_id' ORDER BY p.last_visited $order LIMIT 1");
+        $data = $select_query->fetch();
+
+        $recent_page_name =  $data['url'] . " (" . $data['name'] . ")";
+        return $recent_page_name;
+    }
+
+    /**
+     * @return int
+     */
+    public function totalVisitedPages()
+    {
+        return $this->total_visited_pages;
+    }
+
+    /**
+     * @return string
+     */
+    public function mostRecentPage()
+    {
+        return $this->most_recent_visited;
+    }
+
+    /**
+     * @return string
+     */
+    public function leastRecentPage()
+    {
+        return $this->least_recent_visited;
+    }
 }
